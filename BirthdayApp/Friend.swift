4417fb2fb8089884d9907c2533f2a544012f782f
Nikki <nikkiranabhat@gmail.com>
2025-07-25 13:32:23 -0400

    added a lot of things like birthdays, buttons and more. there is an error rn though

---
+++ BirthdayApp/Friend.swift
@@ -0,0 +1,19 @@
+//
+//  Friend.swift
+//  BirthdayApp
+//
+//  Created by Nikki  on 7/25/25.
+//
+
+import Foundation
+
+class Friend {
+    
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name //assigns name to itself
+        self.birthday = birthday //assigns birthday to itself 
+    }
+}
