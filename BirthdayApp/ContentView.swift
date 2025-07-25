528420ef96fde90a9038373e071e88689106d6b1
Nikki <nikkiranabhat@gmail.com>
2025-07-25 14:19:18 -0400

    added swiftdata to store my things!

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -6,18 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State private var friends:[Friend] = [
-        Friend(name: "Joan", birthday: .now),
-        Friend(name: "Sienna", birthday: .now)
-    ]
+    @Query private var friends:[Friend]
+    @Environment(\.modelContext) private var context
     @State private var newName = ""
     @State private var newBirthday = Date.now
     
     var body: some View {
         NavigationStack {
-            List(friends, id: \.name){ friend in
+            List(friends){ friend in
                 HStack {
                     Text(friend.name)
                     Spacer()
@@ -31,25 +30,26 @@ struct ContentView: View {
                     Text("New Birthday")
                         .font(.headline)
                     DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now,
-                        displayedComponents: .date)
+                               displayedComponents: .date) {
                         TextField("Name", text: $newName)
                             .textFieldStyle(.roundedBorder)
                     }
-                    Button("Save") {
-                        let newFriend = Friend(name: newName, birthday: newBirthday)
-                        friends.append(newFriend)
-                        newName = ""
-                        newBirthday = .now
-                    }
-                    .bold()
                 }
+                Button("Save") {
+                    let newFriend = Friend(name: newName, birthday: newBirthday)
+                    context.insert(newFriend)
+                    newName = ""
+                    newBirthday = .now
+                }
+                .bold()
+            }
             .padding()
             .background(.bar)
-            }
         }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
